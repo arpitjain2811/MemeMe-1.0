060a0a6f73bafcf68cc6de0f199a3d5d5ae5f4c1
Arpit Jain <arpit.jain2811@gmail.com>
2017-01-03 22:12:02 -0800

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Arpit Jain on 1/3/17.
+//  Copyright © 2017 Arpit Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
